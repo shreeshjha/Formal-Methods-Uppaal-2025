893f7f776c1e0daccb67f6c8ba08dd1520f256b3
SamarthBhatia <samarthbhatia700@gmail.com>
2025-04-20 11:52:10 +0200

    minor fixes

--- init_enemies.py
+++ init_enemies.py
@@ -1,30 +1,19 @@
-NUM_CIRCLES = 400
-NUM_SQUARES = 300
+NUM_ENEMIES = 700
 
-hp = [10]*NUM_CIRCLES + [20]*NUM_SQUARES
-dmg = [2]*NUM_CIRCLES + [4]*NUM_SQUARES
-speed = [1]*NUM_CIRCLES + [3]*NUM_SQUARES
+# Generate individual enemy instantiations
+enemy_decls = [f"E{i} = EnemyTemplate({i});" for i in range(NUM_ENEMIES)]
 
-def array_to_init(name, values):
-    lines = []
-    lines.append(f"int {name}[{len(values)}] = {{")
-    for i in range(0, len(values), 10):
-        lines.append("  " + ", ".join(map(str, values[i:i+10])) + ",")
-    lines[-1] = lines[-1].rstrip(',')  # remove comma from last line
-    lines.append("};")
-    return "\n".join(lines)
+# Generate system line
+system_parts = [f"E{i}" for i in range(NUM_ENEMIES)]
+system_parts += ["Monitor", "ClockTick1", "Basic1", "Cannon1", "Cannon2", "Cannon3", "Cannon4", "Sniper1", "Sniper2"]
+system_line = "system " + ", ".join(system_parts) + ";"
 
-print(array_to_init("hp", hp))
-print()
-print(array_to_init("dmg", dmg))
-print()
-print(array_to_init("speed", speed))
+# Combine everything
+output = "\n".join(enemy_decls + ["", system_line])
 
+# Optionally write to a file
+with open("enemy_system_declarations_700.txt", "w") as f:
+    f.write(output)
 
-for i in range(700):
-    print(f"E{i} = EnemyTemplate({i});")
-
-enemy_instances = ", ".join([f"E{i}" for i in range(700)])
-system_line = f"system {enemy_instances}, Monitor, ClockTick1, Basic1, Cannon1, Cannon3, Cannon4, Sniper1, Sniper2;"
-print(system_line)
+print(output)
 
